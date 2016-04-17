973b90e983b0a5e1e9cf2bfbfe30fc578d67eb5d
chris huey <bchuey415@gmail.com>
2016-04-17 12:03:34 -0700

    polylines snap to road w/ hardcoded input

--- src/locations/admin.py
+++ src/locations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from locations.models import IntersectionLatLng, ParkingSection
+
 # Register your models here.
+
+admin.site.register(IntersectionLatLng)
+admin.site.register(ParkingSection)
--- src/locations/models.py
+++ src/locations/models.py
@@ -20,3 +20,60 @@ class Location(models.Model):
 	def __str__(self):
 
 		return str(self.id)
+
+
+class IntersectionLatLng(models.Model):
+
+	lat = models.CharField(verbose_name='latitude', max_length=255, null=True, blank=True)
+	lng = models.CharField(verbose_name='longitude', max_length=255, null=True, blank=True)
+	parking_section = models.ForeignKey('ParkingSection', related_name='latlngs')
+
+	class Meta:
+
+		db_table = 'intersections'
+
+	def __unicode__(self):
+
+		return unicode(self.id)
+
+	def __str__(self):
+
+		return str(self.id)
+
+SECTION_CHOICES = (
+
+	('A', 'A'),
+	('B', 'B'),
+	('C', 'C'),
+	('D', 'D'),
+	('E', 'E'),
+
+)
+
+class ParkingSection(models.Model):
+
+	label = models.CharField(max_length=1, choices=SECTION_CHOICES)
+	time_limit = models.IntegerField()
+	# accepted_permit = models.ForeignKey(ParkingPermit)
+
+
+	class Meta:
+
+		db_table = 'parking_sections'
+
+	def __unicode__(self):
+
+		return unicode(self.label)
+
+	def __str__(self):
+
+		return self.label
+
+# class ParkingPermit(models.Model):
+
+
+
+
+
+
+
