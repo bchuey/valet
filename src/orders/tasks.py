8fec899f8a7f5172337431b27ff641bb5f0770f2
chris huey <bchuey415@gmail.com>
2016-04-20 16:49:47 -0700

    distance and matching algo; refactor watchposition

--- src/orders/tasks.py
+++ src/orders/tasks.py
@@ -8,6 +8,8 @@ from orders.serializers import ScheduledReparkSerializer
 
 import redis
 
+from geopy.distance import vincenty
+
 """
 ==============
 Celery Tasks
@@ -26,11 +28,13 @@ Celery Tasks
 - then next request is up in queue
 
 """
+@app.task(name='query_repark')
+def query_repark(repark_id):
 
-### use callbacks ###
+	scheduled_repark = ScheduledRepark.objects.get(id=repark_id)
 
-
-
+	
+	
 # use <name>.apply_async(eta=) on views.py
 @app.task(name='match_valet_with_repark')
 def match_valet_with_repark(repark_id):
@@ -40,9 +44,28 @@ def match_valet_with_repark(repark_id):
 	valets = User.objects.all().filter(is_valet=1).filter(is_available=1)
 
 	"""
-	Set repark.reparked_by to first valet in list
+	Calculate closest distance in miles
+	Assign the closest valet to repark vehicle
 	"""
-	scheduled_repark.reparked_by = valets[0]
+
+	location1 = (scheduled_repark.pickup_location.lat,scheduled_repark.pickup_location.lng)
+	for valet in valets:
+
+		location2 = (valet.valet_start_pos.lat, valet.valet_start_pos.lng)
+		distance = vincenty(location1,location2).miles
+		if min_distance:
+
+			if distance < min_distance:
+
+				min_distance = distance
+				assigned_valet = valet
+		else:
+
+			min_distance = distance
+
+	scheduled_repark.reparked_by = assigned_valet
+
+	# scheduled_repark.reparked_by = valets[0]
 	scheduled_repark.save()
 
 	# somehow run the 'incoming-request' socket 
